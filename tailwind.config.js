a1a1c5ee3c9295eaa77c6080192f1960b744a1b5
xavir <xaviranik.me@gmail.com>
2021-06-26 14:11:44 +0600

    add: custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: "#0a192f",
+        brand: "#64FFDA",
+      },
+      borderColor: ['active']
+    },
   },
   variants: {
     extend: {},
