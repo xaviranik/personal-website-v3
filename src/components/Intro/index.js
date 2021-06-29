da826aca7359b18d757d3fb6ebe874c06d36c2ab
xavir <xaviranik.me@gmail.com>
2021-06-29 22:10:25 +0600

    add: reveal animation on navbar and intro

--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,23 +1,27 @@
 import { Link } from "react-router-dom";
+import Fade from "react-reveal/Fade";
 
 const Intro = () => {
   return (
     <div className="w-full m-auto max-w-screen-xl text-gray-300">
       <div className="flex flex-col h-screen justify-center items-center -mt-20 px-8">
-        <h1 className="text-brand text-sm md:text-xl mt-8">Hello, World</h1>
-        <h1 className="text-4xl text-gray-300 font-semibold mt-4 md:text-7xl md:mt-8">
-          I am Zabir Anik
-        </h1>
-        <h1 className="text-gray-400 text-2xl text-center mt-8 md:text-5xl md:mt-12">
-          I love to think about <span className="text-brand">ideas</span> that
-          matter.
-        </h1>
-        <Link
-          className="flex border-2 border-solid border-brand px-7 py-3 text-brand rounded-md transform hover:bg-brand hover:text-navy hover:text-2xl font-medium transition duration-700 ease-in-out mt-12 md:mt-24 md:text-xl"
-          to="/"
-        >
-          Let's Hear From You
-        </Link>
+        <Fade bottom>
+          <h1 className="text-brand text-sm md:text-xl mt-8">Hello, World</h1>
+          <h1 className="text-4xl text-gray-300 font-semibold mt-4 md:text-7xl md:mt-8">
+            I am Zabir Anik
+          </h1>
+
+          <h1 className="text-gray-400 text-2xl text-center mt-8 md:text-5xl md:mt-12">
+            I love to think about <span className="text-brand">ideas</span> that
+            matter.
+          </h1>
+          <Link
+            className="flex border-2 border-solid border-brand px-7 py-3 text-brand rounded-md transform hover:bg-brand hover:text-navy hover:text-2xl font-medium transition duration-700 ease-in-out mt-12 md:mt-24 md:text-xl"
+            to="/"
+          >
+            Let's Hear From You
+          </Link>
+        </Fade>
       </div>
     </div>
   );
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,57 +1,60 @@
 import { HiOutlineMenuAlt1 } from "react-icons/hi";
 import { Link } from "react-router-dom";
+import Fade from "react-reveal/Fade";
 import Logo from "../../components/Logo";
 import "./navbar.scss";
 
 const Navbar = ({ toggle }) => {
   return (
     <nav className="flex justify-center items-center sticky transition duration-500 ease-in-out top-0 z-20 h-20">
-      <div className="flex justify-between items-center h-20 z-10 w-full px-8 max-w-screen-xl">
-        <Logo height="h-8" />
-        <div className="md:hidden text-2xl text-brand" onClick={toggle}>
-          <HiOutlineMenuAlt1 />
+      <Fade top>
+        <div className="flex justify-between items-center h-20 z-10 w-full px-8 max-w-screen-xl">
+          <Logo height="h-8" />
+          <div className="md:hidden text-2xl text-brand" onClick={toggle}>
+            <HiOutlineMenuAlt1 />
+          </div>
+          <ul className="hidden md:flex items-center list-none text-md font-medium text-center -mr-5">
+            <li className="h-20">
+              <Link
+                className="flex items-center px-7 h-full text-gray-300 hover:text-brand transition duration-700 ease-in-out active"
+                to="/"
+              >
+                About
+              </Link>
+            </li>
+            <li className="h-20">
+              <Link
+                className="flex items-center px-7 h-full text-gray-300 hover:text-brand transition duration-700 ease-in-out"
+                to="/"
+              >
+                Projects
+              </Link>
+            </li>
+            <li className="h-20">
+              <Link
+                className="flex items-center px-7 h-full text-gray-300 hover:text-brand transition duration-700 ease-in-out"
+                to="/"
+              >
+                Blog
+              </Link>
+            </li>
+            <li className="h-20">
+              <Link
+                className="flex items-center px-7 h-full text-gray-300 hover:text-brand transition duration-700 ease-in-out"
+                to="/"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+          <Link
+            className="hidden md:flex border-2 border-solid border-brand px-5 py-1 text-brand rounded-md hover:bg-brand hover:text-navy font-medium transition duration-700 ease-in-out"
+            to="/"
+          >
+            Resume
+          </Link>
         </div>
-        <ul className="hidden md:flex items-center list-none text-md font-medium text-center -mr-5">
-          <li className="h-20">
-            <Link
-              className="flex items-center px-7 h-full text-gray-300 hover:text-brand transition duration-700 ease-in-out active"
-              to="/"
-            >
-              About
-            </Link>
-          </li>
-          <li className="h-20">
-            <Link
-              className="flex items-center px-7 h-full text-gray-300 hover:text-brand transition duration-700 ease-in-out"
-              to="/"
-            >
-              Projects
-            </Link>
-          </li>
-          <li className="h-20">
-            <Link
-              className="flex items-center px-7 h-full text-gray-300 hover:text-brand transition duration-700 ease-in-out"
-              to="/"
-            >
-              Blog
-            </Link>
-          </li>
-          <li className="h-20">
-            <Link
-              className="flex items-center px-7 h-full text-gray-300 hover:text-brand transition duration-700 ease-in-out"
-              to="/"
-            >
-              Contact
-            </Link>
-          </li>
-        </ul>
-        <Link
-          className="hidden md:flex border-2 border-solid border-brand px-5 py-1 text-brand rounded-md hover:bg-brand hover:text-navy font-medium transition duration-700 ease-in-out"
-          to="/"
-        >
-          Resume
-        </Link>
-      </div>
+      </Fade>
     </nav>
   );
 };
