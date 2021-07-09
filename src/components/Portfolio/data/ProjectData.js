db46a4d742f251797b569bd2c19e4f86c12372fa
xavir <xaviranik.me@gmail.com>
2021-07-09 22:14:35 +0600

    update: projects data

--- src/components/Portfolio/data/ProjectData.js
+++ src/components/Portfolio/data/ProjectData.js
@@ -12,16 +12,13 @@ export const ProjectData = [
     },
     images: [
       {
-        image:
-          "https://lh3.googleusercontent.com/-wXtlJ8IJkJ0c74QmFVlJyImMujeH_AZSK1kE65BdqB6WVuW1e3-DWOHZEvIlSBAyiH4Hh07pRRQimbFzV5w5FDiS4gYYkeMSmiDjj8Qe2YPR7CUHIfJ2SKCbhYCaZG_ubc1QTFa9z_8N4xKlOB5PvmWkPhO60hH8ul27VyKuZxGtUpIz8HvkJ5N_SgZYv0OXWHGPUm7UIJnwtKqcTID3rxbzByTc6n5i0yjuC-Ce9FHlQQ3pQ5ZMptdMWAbmMzTRg4ZWP6v6hQae5ZWFTGcs56SFZiXxYuxAEuCod8FtaNljrfGzg7jKS1tAqHwmmM_wTTt_NrmytnLZZd6kx76GT0_2lwYWE019bWOMuoOsWJdSMJSaNRS2ANB1zeWtw-uHbfutLP7Lkt7IiokfnyCksRk9zxZ_-Wa5AQw70YFRO-cDcGDfGEVSimcKHsSaet3UZpad0WNeHOPp0hnlYA-pFrvuO3Mvp4qW8ecOk5124NlhvCJWmHM8zbnj57YaAKLLKXKCzy09cl860eWFf_j-npX2TEE4AZwCaGBxcXj1L1Bqh4EV9uca1g3OeMiw_A4rCB1C1WHmmXZ3kO3_sht_kbeZCdTGfiUhTu6TUpwyPXMWOP-cgZj27KOe9a0VFpTP-6FxEWs541QOmWwcx-QtBrL5MlMcU-y4d8mKnClvxY4qRM0xZBAGi7DI1xeuHoh2_eypoYTDFNOF3mRR7sX3WQ=w1661-h941-no?authuser=0",
+        image: "https://i.postimg.cc/tRVg0V7M/curlycamp-1.png",
       },
       {
-        image:
-          "https://lh3.googleusercontent.com/k_PNUrNpzZ_Ke8hexFOIHVuO-iaz9GJUaZoqAdtQcEIGpgRzibGdEjgqGd9LZIBW4e1tOZeRHshQgGU-o0OiQ-UGgpPMX9-imhJ7uKOFbfbVmYKxBDb12OSyLhkNRTcf8mnuJ-A9DBExszzYQgF3Mn4HuKmwnsXO0Cx-vddVLZfR2KgSMXLaxgv1yXD5oi51syP9j3Lkmty7xVHdx1kI-ZIwe6fBg0RMyRBtYaUVXZHyMdKiKMQSKWvcBqHSlCcpCkmX-JGsJ4q-IFKLs_WlM7d3oH-ELA3b7rN5KsiDGZ3R6_3HkobkW6NN2ktLyWgSkxVB-EzMUI0ZGcua-fKg5U9mOkKHu6Ql_HETnGRyr6KupAwUgdEBj2nSWUbha3Rl2rXwB6cOCNm5vsR04iDwBrPNG80sRhEl1XUG5rcOJBpUSndQxmEI7T7kbWDA1EN7268ftCJAVs_bxzYkN0hdoI2mdZosO5rYf4hYkB7PpdShwemdUZTd2OdnvMI_6nR2DMvbC9hTm7ELmAA1ZIWY8kjx6Dg44OgkGsfxNWsEJR9Wen1X5_91bkthVQRmfkFH33oTkY7Ka5WhxQeov65p_5pkNn6tDc4X13gc209JgivFSn30Hzk16GyqHUhwj4bTgc3JHp2Lgp3zcbSTZ7kWpgRHBNPOHwaFM35wWyGI2h8DAv6CtxSDGSyspFrSsoftkruUI32_rsHSkMcwS1LSn1w=w1659-h942-no?authuser=0",
+        image: "https://i.postimg.cc/7Y8ZqY2Y/curlycamp-2.png",
       },
       {
-        image:
-          "https://lh3.googleusercontent.com/rjV6tI8Sjvakk-OBdGMqgHFseAt540lvEe_i0MVevCyne4BruMk_jCTOhwmymk1wqoi15Q4tgUXLFUFN154Aq63fALt8W7gJLBFrbQexZnDzqoZNjCjf6b9sRBgNIfD9S1Z9ZOVinKlP2K-9GinEBtU2GBVDMBluh0cV6310AS-3Asad2qhK5KKudOddXOWxJ9DsdPwShzlVqiSDJhrzzBDLF9rpION3kN0sNn3sNIHUp0ckFjg5g-qKoHmJ9rsc5K955ohsuSRCZGo5R9yeu-q0SzoNhHx_spEAdx8IyfMugaQafcRGkFsLgw85VQAjr4dy6BJPiBb1Sdao8pzgsDxlEtKhOzAB4vtXPr3nCeiQ93R2qg1hD6p61xnlilnQ2VD1cPt7IiximnFP7WYLaEz7JsGVY8ufWtAdNB-0MlltsDvvGvBXP13TPgIx9diOKxBhcTSr-32CBYfmndcUd26YsqWR8cTC12LPuSRpVJMard5eah9ecUhWTr44o0Vk87blO4QGqFNdaLaOdkDDc8f2TgrnVuhHRgsiGQJRs0peI8AvfrNu8cImYYobisRQ7wotvJ_MsTXant-lK7iork9VO7X8cVh2G_itKWoNbLQ8PuD-TOPQ1QcbwAgtGucwg4RtSw-_QiQlsI8bNZfIkHu7Ojd9f0gZH9-xW91HgEPs396V_IlKyXSTIYGJHk4jMfuYw-GQNEVUlVc1AdtrzsQ=w1658-h941-no?authuser=0",
+        image: "https://i.postimg.cc/fyXWY16N/curlycamp-3.png",
       },
     ],
   },
@@ -38,16 +35,13 @@ export const ProjectData = [
     },
     images: [
       {
-        image:
-          "https://lh3.googleusercontent.com/h1fD8gxIoN33xpz1NZf28JJtUBC-WS7At4uwPjs9pAYvY6eguGyI-evQ-od7XourkSV7tsctISQAyyGcW5193alrREUs0WAMHa6O0HE8p6WPyLPRTKJVi-pvHQBOfzLmQGsh2OOP7GS22ZEKdUEOoAPi7U6VrEtIMoZDTPVD4FMwNxI4kqLDqpGCs1UUCFNUYcBFTJ2FHuLuS9otPINOGvQVfaMQe-nRmwvuB4Ps-Ev5DwGpBFdil5VzCzyBz6J3emdjErWIQ7ZT6LMjmIMxaCIkBuBBfVuIYHz3enwbLpJaDSIalRfQKonmz-6EsQEJP4DT5YFdiQ2cSOaE93BLkJF7NGC3rEaZbQsgzrVpJpYmInYDnM9x0AG-hWFKSyy3HbnTaPTgpUgibda-dCO82amowHyd5wCQAwbaPmixMYkrSa97-NqyNKVZxa-KBxKX4_Zs1fP71j-R50aA2x7-L7KKeogtseEA9i88nEq_rBDWlyu-mT4fqajVAhD0oOi_yMeiX4mIotSgVErReDO5ey2PpH6L_KI91WwN9hh-zGMXs5H4Hd8ACUkBnfM3EziiTVicj7QaYzGxLX0VJrV2JhOuZ4Th_HATS9I-qIolMR_1slUbefgY2jKpY4zFVpnc8sdcHOIEKLZTdxF1-PiSmapM_9prt_NOTekdb8xfCELEWdm3zA1PHT3GqXZzj7-73V3hJ2NsqHDylnijsvho_FM=w1659-h942-no?authuser=0",
+        image: "https://i.postimg.cc/pTz3CxXq/devvit-1.png",
       },
       {
-        image:
-          "https://lh3.googleusercontent.com/iyX0WiuAoDx6WDm7_kzwggySUYRU0PtlJ4oYpOiAbf7K-NXrwLns4WltzarZ5LqVxevZzo3pvQ7hUk5UrMT1UcOoSzKbXZTXBdHtWhMEc1ttKYrricu-tnL26EyVip1qSF_mbeMSAW8JLBJ4szlH027q6aQBPcOCvTnn6f9szOwZMoYe7iEpObZiMxfPvVajS6ITGh-jYJffCIWxNVXP15mrSZflQwY6EEHWQMBtNazik6AxpgcShyr9iHq_CNGEkZK0RypB25FfGWrQevyIMG4tBQaUmattap1RKZnXGjxg0ia5kvV-j0AsNzCTKL6zg6SDvSxGTbPWEh6O8N0Z4sTaOFF1JWEwLVf_jIdZbZP_RkQd-AU4pp_1hDlFSnurtbZRO6AJwkaRXrZgTakJuZi0gsrAJgcCVCFAu2_qD5mCwrMg6r7fr8VbWKw9yNdgmCeP3oKvd1OZ6wzQnzyP8n-WZ6llqI-koHlvNxcCY28gk6BCObNanpmQBV8w4_ruugTzl7S5DomowS4mhScyDGgQp4ON_p2qbuWoWSVJsgYjMcbNje6PtYpsnr0vcHqR09ankEThcDaSGTWohyx9j3Hj-_sOioq8bpDQgpYfcEVOqQmU6zF0umVg7jkSIf0L_LA4bqYRznsSf6_w1e5UD05f10TofnkqE8rMIBuxygqTzrgHEoQ1VLjcPJ4HvKOUeiJ5Mj0a5FbAx1hFwkHn5M4=w1658-h941-no?authuser=0",
+        image: "https://i.postimg.cc/5yJTkGX1/devvit-2.png",
       },
       {
-        image:
-          "https://lh3.googleusercontent.com/pgHLGNzeCaMRMeRJXz8GW_B4idUshVya9ECO2HMLB6z5eZSCBlWN9eVe0VK7gi7cpRE3PZcNUBtyYTebkUic62rfKFTCNZofPKv_csF4WNP6_6oT3hnQRRWbGvMewYNqU9FQ-ZTKTFpDt0YIE7OLkR9XqOWRoGJQtxASo4Ls1VuI2E4lZZRJy0dRPayr0rmZE8gecXR9MjX6FqW9rxGbvooRnmcrBcn0vks5_fy-TqRdQbopSgsovYYMevkaDKT7uHzdL3QMoFbQzW4KZYc21iLY9edxNcmhEnf1fWA4xgK3g1sji8ZPbfK4vhoqj4FrTBib8ZN_25fEt3ApuP6hoSLayiiE3BzNamIj1qaxnCn1rJM8XbqlX91bQegRP79B3eIFG9pNDxbBreltEwQA3cQT13uQVaXYG3cHCXHKsxE3yP1tqbqkm1Zkszmdqkvfh8aqT17NvaSJ5lrzePDgJYG-_-KcxHrS6kQXnV46jAq7Y9u6KAGvKAmwbiccN2g7RumU56FI4_nOlw67BlHYcpzFdxM1szehhmyzmRxNrSR858MZnp4modNUYwh8W9s_sGyJ4heO8vc_dJjuHoO5yTwH2n39ymu4TcpQDeCE_kfjdeHkbRfH3SsCfkL1NDcF3tcy0mupE58Obx5MwwLhIlWe_SiDZcTN0l-eEfrHRq6n1VOhpHkpij8bkQRV8lJJ8uj_9WnK8OZ9MaVrkLvHu3I=w1659-h942-no?authuser=0",
+        image: "https://i.postimg.cc/W4QQ0tjd/devvit-3.png",
       },
     ],
   },
@@ -64,16 +58,13 @@ export const ProjectData = [
     },
     images: [
       {
-        image:
-          "https://lh3.googleusercontent.com/Julh5zaamNIYgfXtzmnz09VWGatUpojhs-VB4qKXl4TasdFd1nhQysikU0hoK0WJKOkjbJcztepRx4GgI9htW4xs6D8rh7W62d1YSWJIeGDNag2ncdZCbabOh0-Sl8WGwQ_5vuw9cQg5oN120VBoJj5B5_nYqfEMGMxCZIVVQYsU2NfaTeWVxubDjwx3cRQUSwerXpCMOBi5E7zqxqP6OvRAawfL93YNgjUlrmGjaH8jvzVbbkPcza13FfX20srpaH2bA8oR5DkeF6EiTZjWxjoYiwCqtlZ4dGAfhcMtgMNWCX1UfQ_qAljfjVRPBykyn0w2uwM1XfJCanoksX21dFAn6rbT1V_0Dvb-dH8mTifHa6jVT2znngbLt07UXfydmf0MzcveIQGXqimwtH5UIh_EiWttsbtBFP-I425YOztMdgn7xM2ROlajE43vRhYXHzE2m_hnWSgeKUJJt0mKcr1Ahwxff4e6JSvCSaISWdVTNDgXQ89b0CwIvEFbGKnERNj2XJMdiicWmdJL73RHoT2oXmSzhtQq1deOOsQ5NNt4SKL-zLJG7fmYbxJ0rAXOglKWadMB6zErxIl0DXvLrnFqPU-ffws9MyllRdxM_Cj_wogB1Hy5AhEBg_bVRcodsSdwUjj5uygQfGFfSTBxKRqIEHPVxSWVsVnZdJy1vgEdEpTx6C8k2yMF0IN0QbqsmjFHHneC1MR3GgCz_kHsdxM=w1662-h941-no?authuser=0",
+        image: "https://i.postimg.cc/Hsy8thQj/lifehacks-1.png",
       },
       {
-        image:
-          "https://lh3.googleusercontent.com/kgRzURO9l3UdAG7yGboNLK3WcODrpce6nKA9ffIkzEDVMxDlKjGEQSBGZBrzFnbkds2LHb1WMC6htXldfmFIbTZDqrSw3oMDHylP9qv0ruU5LqqHkgJw4QGbiuMq_lL86XT7s4gX_Binrerrcgah4ArZdAQTNmXoXN31cKfHdinuavEr6s9V261sRqWG9HtqhlN_29ZRPE1vwtWJh_3yYRZoYlHAwEcLtsoniSzC2SiJQtiUlMcZxr4USD5a5mIpQB0AOLDGPcisUlJDQ4a0F_Gjj7pneZNcOAfiZGD_jUzn2aRzJGtj0mAleQCfPFWJb0EQ3QCkyJOA9G9nmYG5xYYLUVfyY91HD_dCetNAyNGSdhPGT7ZqtQT9CUhaA8uBECWw-GT7LOs12jEP3J11ToH3S7Nh8GK5NzReyYWgxAjOzkCT43k2pl_-ZWkI4l1QRzIOOoxr1YXqEXaPaA_FBU9vABSK741Ei4-aa4rF7tTYIrAnIllI1Thyn7VX21jZ-aUQL9fXmAhr8g-AfpQZLiaBtW9XdIoMOgd_S5WJqyEOvVEvPr-0J58TNcn0d7c22nqQti_3BN4ePDN5SXr0FUFWYnvBUzAgpgJYsrVjCWEdWmjFNVp06W80-QzZ_VzX_M-_KReXMwjsImT7hE-wkbcKqDUMgISwOy5r8N8kdm_OFiXI0QzO8EjLKP40DN6aSKl4nrDTxuV6rycLagd-jbI=w1661-h941-no?authuser=0",
+        image: "https://i.postimg.cc/hvHzBLqS/lifehacks-2.png",
       },
       {
-        image:
-          "https://lh3.googleusercontent.com/_UkQ1ERmCkB9-Es-x3VlEWEABXTsvfxve9GVAHJrKTMLPNyhMyaAMEdwx2ZOBc6yvnxXu9FHDSgC9d7Nmt5LxrFNKr1MvJAQQuCOCkcN8AeQAqpn3hnqY3UHtcBH-OBYV5BZ1S0RVdEVhUc35i03Inw79kQFbSpG-C3qvSD7Vh2WvrC4vJOB3xwCxcBtVqhBcWAWjThCAU5G2niL8fCkWz9Jd-UY9WCtvZGDtFMXLYG4c6uyUJSDlrQ29e4I4WcRPy6ewC8XcdRakEqP6GeHfQjKYqAIJd9G64wf8zoZzWMD1xZrIKR3wlLLNPGD1GJyiZ_lb7sj7LQpmiDsWZzNxr10osDSTJmiOQFioFUOi8PXIWMsR7zeKGvjNm9wC1xY52f1tSc8c2K6815hijeuKubOQfNIkCz2CGJCn9B6Ox8WT8BoQyYS8598ccUv2BiXMOteYZHp61bsqa-8ut02-D-aoWU799EkJr6c35u_tfnTc2C8i-Smm3OmI0wv0NUP8LipwPcvSzgJWzBKUy4HK1KF5Dx2iWJneAwXF8SiXQyrJTYidHigxDHDIZwZAxM03xM2Et-ziDnUCg6O_4XC1E0GhHpj-7CofrRt4R5wPoMcppznBymB1B9Z517MIyJiaT4nH9CBCxTzww5N9Pve9wo7ZkMA-S3jBQPuHWQnjyRSJWdq73JBZ5NINewwqJm1pym_d9YALdfr5oAdE31T2Os=w1659-h942-no?authuser=0",
+        image: "https://i.postimg.cc/RC8nKBJj/lifehacks-3.png",
       },
     ],
   },
@@ -92,32 +83,6 @@ export const ProjectData = [
   },
   {
     name: "WellBeing Pharmacy",
-    thumbnail:
-      "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
-    description:
-      "Project Management Application for task managements and collaboration",
-    tags: ["Laravel", "MySQL", "Vue.js", "Heroku"],
-    links: {
-      github: "https://github.com/xaviranik/curlycamp",
-      external: "http://ggcurlycamp.herokuapp.com/",
-    },
-    images: [],
-  },
-  {
-    name: "MyNUB",
-    thumbnail:
-      "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
-    description:
-      "Project Management Application for task managements and collaboration",
-    tags: ["Laravel", "MySQL", "Vue.js", "Heroku"],
-    links: {
-      github: "https://github.com/xaviranik/curlycamp",
-      external: "http://ggcurlycamp.herokuapp.com/",
-    },
-    images: [],
-  },
-  {
-    name: "MyNUB",
     thumbnail:
       "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
     description:
