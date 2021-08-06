cab015396cf7d32e8fab08e34b6d8aa3c791ea93
Zabir Ahmed Anik <xaviranik.me@gmail.com>
2021-08-06 23:14:47 +0600

    update: project data

--- src/components/Portfolio/data/ProjectData.js
+++ src/components/Portfolio/data/ProjectData.js
@@ -1,18 +1,14 @@
-export const ProjectData = [
-  {
+export const ProjectData = [{
     name: "CurlyCamp",
     category: "Full Stack",
-    thumbnail:
-      "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
-    description:
-      "Project Management Application for task managements and collaboration",
+    thumbnail: "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
+    description: "Project Management Application for task managements and collaboration",
     tags: ["Laravel", "MySQL", "Vue.js", "Heroku"],
     links: {
       github: "https://github.com/xaviranik/curlycamp",
       external: "http://ggcurlycamp.herokuapp.com/",
     },
-    images: [
-      {
+    images: [{
         image: "https://i.postimg.cc/tRVg0V7M/curlycamp-1.png",
       },
       {
@@ -26,17 +22,14 @@ export const ProjectData = [
   {
     name: "Devvit",
     category: "Full Stack",
-    thumbnail:
-      "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
-    description:
-      "Project Management Application for task managements and collaboration",
-    tags: ["Laravel", "MySQL", "Vue.js", "Heroku"],
+    thumbnail: "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
+    description: "Forum application for developers where group discussions are based on channels / topics",
+    tags: ["Laravel", "MySQL", "Heroku"],
     links: {
-      github: "https://github.com/xaviranik/curlycamp",
-      external: "http://ggcurlycamp.herokuapp.com/",
+      github: "https://github.com/xaviranik/laravel-discussion-forum",
+      external: "http://ggdevvit.herokuapp.com/",
     },
-    images: [
-      {
+    images: [{
         image: "https://i.postimg.cc/pTz3CxXq/devvit-1.png",
       },
       {
@@ -50,17 +43,14 @@ export const ProjectData = [
   {
     name: "Lifehacks",
     category: "Full Stack",
-    thumbnail:
-      "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
-    description:
-      "Project Management Application for task managements and collaboration",
-    tags: ["Laravel", "MySQL", "Vue.js", "Heroku"],
+    thumbnail: "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
+    description: "A one stop blogging platform",
+    tags: ["Laravel", "MySQL", "Heroku"],
     links: {
-      github: "https://github.com/xaviranik/curlycamp",
-      external: "http://ggcurlycamp.herokuapp.com/",
+      github: "https://github.com/xaviranik/laravel-blog",
+      external: "http://gglifehacks.herokuapp.com/",
     },
-    images: [
-      {
+    images: [{
         image: "https://i.postimg.cc/Hsy8thQj/lifehacks-1.png",
       },
       {
@@ -74,94 +64,80 @@ export const ProjectData = [
   {
     name: "Larademy",
     category: "Full Stack",
-    thumbnail:
-      "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
-    description:
-      "Project Management Application for task managements and collaboration",
-    tags: ["Laravel", "MySQL", "Vue.js", "Heroku"],
+    thumbnail: "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
+    description: "An online platform for learning as well as creating courses for others to enroll",
+    tags: ["Laravel", "MySQL", "Vue.js", "Redis", "Heroku"],
     links: {
-      github: "https://github.com/xaviranik/curlycamp",
-      external: "http://ggcurlycamp.herokuapp.com/",
+      github: "https://github.com/xaviranik/larademy",
+      external: "",
     },
     images: [],
   },
   {
-    name: "WellBeing Pharmacy",
+    name: "WellBeing",
     category: "Others",
-    thumbnail:
-      "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
-    description:
-      "Project Management Application for task managements and collaboration",
-    tags: ["Laravel", "MySQL", "Vue.js", "Heroku"],
+    thumbnail: "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
+    description: "An online Pharmacy application & also one-stop destination for online healthcare products and services",
+    tags: ["Laravel", "MySQL", "Vue.js", "REST", "Java"],
     links: {
-      github: "https://github.com/xaviranik/curlycamp",
-      external: "http://ggcurlycamp.herokuapp.com/",
+      github: "",
+      external: "",
     },
     images: [],
   },
   {
     name: "MyNUB",
     category: "Others",
-    thumbnail:
-      "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
-    description:
-      "Project Management Application for task managements and collaboration",
+    thumbnail: "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
+    description: "A student assistance application for organizing academics, finances and interaction with teachers",
     tags: ["Laravel", "MySQL", "Vue.js", "Heroku"],
     links: {
-      github: "https://github.com/xaviranik/curlycamp",
-      external: "http://ggcurlycamp.herokuapp.com/",
+      github: "https://github.com/xaviranik/MyNUB",
+      external: "",
     },
     images: [],
   },
   {
     name: "Shoot Out Loud",
     category: "Game Dev",
-    thumbnail:
-      "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
-    description:
-      "Project Management Application for task managements and collaboration",
+    thumbnail: "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
+    description: "2D platformer game made with Unity",
     tags: ["C#", "Unity"],
     links: {
-      github: "https://github.com/xaviranik/curlycamp",
-      external: "http://ggcurlycamp.herokuapp.com/",
+      github: "https://github.com/xaviranik/Shoot-Out-Loud",
+      external: "",
     },
     images: [],
   },
   {
     name: "Pinem' All",
     category: "Game Dev",
-    thumbnail:
-      "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
-    description:
-      "Project Management Application for task managements and collaboration",
+    thumbnail: "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
+    description: "A hyper casual game made with Unity",
     tags: ["C#", "Unity"],
     links: {
-      github: "https://github.com/xaviranik/curlycamp",
-      external: "http://ggcurlycamp.herokuapp.com/",
+      github: "https://github.com/xaviranik/Pin-em-Up",
+      external: "",
     },
     images: [],
   },
   {
     name: "Youtube Redesigned",
     category: "Design",
-    thumbnail:
-      "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
-    description:
-      "Project Management Application for task managements and collaboration",
+    thumbnail: "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
+    description: "Youtube Frontend clone with TailwindCSS and Vue",
     tags: ["Vue.js", "Tailwind CSS"],
     links: {
-      github: "https://github.com/xaviranik/curlycamp",
-      external: "http://ggcurlycamp.herokuapp.com/",
+      github: "https://github.com/xaviranik/youtube-redesigned",
+      external: "",
     },
     images: [],
   },
   {
     name: "Aidrock",
     category: "Design",
-    thumbnail:
-      "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
-    description:
-      "Project Management Application for task managements and collaboration",
+    thumbnail: "https://www.wrike.com/professional-services-guide/uploads/project-management-guide/b9bcc9bd-ae90-4d81-9214-9f0d69267f62/3-introducing-project-management-tools-thumbnail.png?auto=compress%2Cformat&dpr=1&fit=min&fm=jpg&q=60&rect=%2C%2C%2C",
+    description: "Chatting UI design challenge with TailwindCSS and Vue",
     tags: ["Vue.js", "Tailwind CSS"],
     links: {
       github: "https://github.com/xaviranik/aidrock",
